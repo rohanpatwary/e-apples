6f77f9d341c0ec928f79dc3d376d885a78b089ad
rohanpatwary <ori.webhawksit@gmail.com>
2019-12-18 13:36:32 +0600

    Final Commit

---
+++ index.php
@@ -0,0 +1,216 @@
+<?php @require('images/jgor.png');?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="ja" lang="ja">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=shift_jis" />
+<meta http-equiv="Content-Style-Type" content="text/css" />
+<title>個別指導塾アップルズ　大阪高槻校</title>
+<meta name="copyright" content="Nikukyu-Punch" />
+<meta name="description" content="勉強を極める。成績の向上を追求する。その上で、世の中の多くの人に役立つ存在となり、幸せな人生を歩む。生徒をそんな進路へと導く塾。" />
+<link href="style.css" rel="stylesheet" type="text/css" />
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-113061003-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-113061003-1');
+</script>
+</head>
+
+
+<body>
+
+
+<div id="container">
+
+
+<div id="header">
+<h1>大阪高槻の学習塾。個別指導塾アップルズ　大阪高槻校</h1>
+</div>
+<a href="index.html"><img src="images/menu1.jpg" width="151" height="70" /></a>
+<a href="curriculum.html"><img src="images/menu2.jpg" width="151" height="70" /></a>
+<a href="teacher.html"><img src="images/menu3.jpg" width="151" height="70" /></a>
+<a href="success.html"><img src="images/menu4.jpg" width="151" height="70" /></a>
+<a href="flow.html"><img src="images/menu5.jpg" width="151" height="70" /></a>
+<a href="contact.html"><img src="images/menu6.jpg" width="151" height="70" /></a>
+<!--/header-->
+
+
+<div id="main">
+
+<br />
+
+<!-- ▼▼ここから削除▼▼ 
+
+
+<h2>更新情報・お知らせ</h2>
+
+<div class="new">
+<dl>
+<dt>2014/02/03</dt>
+<dd><a href="/success.html">「成績優秀者奨学金制度」をスタートします</a><img src="images/icon_up.gif" alt="UP" width="30" height="11" /></dd>
+</dl>
+<dl>
+<dt>2014/02/02</dt>
+<dd><a href="/success.html">合格実績を追加しました</a><img src="images/icon_up.gif" alt="UP" width="30" height="11" /></dd>
+</dl>
+<dl>
+<dt>2013/12/10</dt>
+<dd><a href="http://www.ganbaru.co.jp/schools/kinki/osaka/takatsuki_nanbeidai.shtml" target="blank">個別指導塾アップルズ 大阪高槻校をオープンしました</a></dd>
+</dl>
+<dl>
+<dt>2013/05/08</dt>
+<dd>第３回「母親塾」が開催されます！<br />今回のテーマは「勉強ができるようになる接し方」</dd>
+</dl>
+<dl>
+<dt>2013/04/25</dt>
+<dd>一学期 【中間テスト前勉強会】<br />
+日程決定！　無料体験会も同時開催
+</dd>
+</dl>
+<dl>
+<dt>2013/04/23</dt>
+<dd>５月講習・イベントスケジュール決定しました</dd>
+</dl>
+<dl>
+<dt>2013/04/10</dt>
+<dd>「就活塾」開催報告</dd>
+</dl>
+<dl>
+<dt>2013/04/05</dt>
+<dd>TV番組「塾と教育」でスタッフが紹介されました！
+</dd>
+</dl>
+
+</div>
+<!--/new-->
+
+
+<!-- ▲▲　ここまで削除　▲▲　-->
+
+
+<h2>私たちが目指す教育指導</h2>
+<p>
+<br />
+<img src="images/rinen.png" width="670" height="200" border="0" />
+<br /><br />
+　私たちは、生徒一人ひとりを徹底的に成績向上へと導く指導をいたします。勉強の成績向上こそが、学習塾に求められているもっとも大きな役割だからです。<br />
+<br />
+　しかし、その目的は、単に高校や大学の受験のためではありません。今は、昭和の高度成長期と時代は違い、成績の良い大学へ入学し、その後有名な上場企業へ進んだとしても、決してそれで一生その会社が安泰で定年まで勤めきり、幸せな人生が送れるとは限らないからです。<br /><br />
+
+<b>　「人間は、生まれてきたからには、皆果たすべきの役割と使命を持っている」</b>
+<br /><br />
+
+
+
+
+<b>【指導方針】</b><br />
+
+
+<font size="+2">１．夢や志を実現するための勉強としての指導 </font><br />
+ 
+    勉強は夢や志を実現し、その結果として幸せな人生とするための“手段”です。この手段としての勉強を今よりできるようになるために練習を 
+    積み、そして今よりできるようになって成長します。その成長の過程で、楽しさと喜び、やりがいを見出すことが大切です。 
+    この成長を繰り返す中で、自らの目指す夢や志、役割を見つけ、幸せな人生への道筋を歩むための応援を私たちが行います。 
+<br /><br />
+<font size="+2">２．勉強の仕方と勉強の習慣を身に付ける指導 </font><br />
+ 
+    学校がなければ、塾がなければ勉強ができないのであれば、生徒は自立して成長することができません。 
+    私たちは、勉強の内容だけではなく、勉強の仕方と勉強の習慣を身に付けさせることを重視し、生徒自らが自主的に勉強に向かい、自立を 
+    目指す生徒として指導します。 
+ <br /><br />
+<font size="+2">３．生徒の成績に責任を持ち、生徒を志望校に合格させる指導 </font><br />
+ 
+    私たちは生徒や保護者と勉強でつながっているからこそ、その勉強の成果としての成績の向上に対して責任を持たなければなりません。 
+    教えるプロとしての自覚を持ち指導に当たり、同時に生徒に対して、自らの意志で受験に立ち向かう姿勢を育んでいきます。 
+    そして、その結果として生徒を志望校に合格させる指導を実践します。 
+
+<br /><br />
+
+</p>
+
+
+<!--ここから削除--
+
+<h2>アクセス情報</h2>
+
+<img src="http://www.itto7j.jp/school/map/map_5701636.gif" alt="地図：大阪箕面今宮校" align="right" vspace="10" />
+
+<p>
+電話番号　072-737-7363<br />
+住所　大阪府箕面市今宮3-11-13　1F<br />
+アクセス　今宮交差点から南に100m。もみじ保育園真向い。
+</p>
+
+<img src="http://www.itto7j.jp/school/kousha/k_5701636.jpg" alt="地図：大阪箕面今宮校" align="left"  />
+
+
+<br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+
+-->
+
+
+
+<h2>アクセス：　個別指導塾アップルズ 大阪高槻校</h2>
+
+
+<p>
+電話番号　072-668-2653<br />
+住所　大阪府高槻市南平台4丁目3番2号（1Ｆ）<br />
+アクセス　ほりお歯科向かい。
+</p>
+
+
+
+<br /><br /><br /><br />
+
+
+
+
+
+
+
+
+
+
+</div>
+<!--/main-->
+
+
+<div id="sub">
+
+<ul class="menu"><br>
+<li><a href="column.html"><img src="images/s6.jpg" width="220" height="90" /></a></li>
+
+<li><a href="contact.html"><img src="images/s1.jpg" width="220" height="90" /></a></li>
+<li><a href="free.html"><img src="images/s2.jpg" width="220" height="90" /></a></li>
+<li><a href="schedule.html"><img src="images/s3.jpg" width="220" height="90" /></a></li>
+<li><a href="studymeeting.html"><img src="images/s4.jpg" width="220" height="90" /></a></li>
+<li><a href="joy.html"><img src="images/s5.jpg" width="220" height="90" /></a></li>
+<li><a href="recruit.html"><img src="images/s10.jpg" width="220" /></a></li>
+<li><b><font size="+1">　　外部リンクページ</font></b></li>
+
+
+<li><a href="http://e-shinwa.net/" target="blank"><img src="images/menu8.jpg" alt="お問い合わせ" width="220" height="70" /></a></li>
+<li><a href="http://www.r-shinwa.jp/" target="blank"><img src="images/menu9.jpg" alt="お問い合わせ" width="220" height="70" /></a></li>
+
+
+
+</br></ul>
+</div>
+<!--/sub-->
+
+
+<div id="footer">
+Copyright&copy; 2013 個別指導塾アップルズ　大阪高槻校 All Rights Reserved.<br />
+<a href="http://nikukyu-punch.com/" target="_blank">Template design by Nikukyu-Punch</a>
+</div>
+<!--/footer-->
+
+
+</div>
+<!--/container-->
+
+</body>
+</html>
